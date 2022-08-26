18f621d14c4b45f3e7c3cd1d00b86449aba61648
Romain Trotard <romain.trotard@gmail.com>
2022-08-26 07:13:55 +0200

    Use the right env variable for the domain name

--- packages/redis-timing-tracker-web/utils/siteUrl.ts
+++ packages/redis-timing-tracker-web/utils/siteUrl.ts
@@ -1,5 +1,5 @@
 const isProduction = process.env.VERCEL_ENV === 'production';
 
-const siteUrl = isProduction ? `https://www.${process.env.NEXT_PUBLIC_DOMAIN_NAME}` : 'http://localhost:3000';
+const siteUrl = isProduction ? `https://www.${process.env.VERCEL_URL}` : 'http://localhost:3000';
 
 export default siteUrl;
