7518cdbc1d71f2a0dd398932a5f2e39800e90ea4
Romain Trotard <romain.trotard@gmail.com>
2022-08-26 08:42:34 +0200

    Trying to make siteUrl works

--- packages/redis-timing-tracker-web/utils/siteUrl.ts
+++ packages/redis-timing-tracker-web/utils/siteUrl.ts
@@ -1,5 +1,5 @@
 const isProduction = process.env.VERCEL_ENV === 'production';
 
-const siteUrl = isProduction ? `https://www.${process.env.VERCEL_URL}` : 'http://localhost:3000';
+const siteUrl = isProduction ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
 
 export default siteUrl;
